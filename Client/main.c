872433fb40a6bb0b0a3f9f05dc7687543a7bfbcf
Nikoleta Dimitrova <nikoletad@vmware.com>
2020-05-12 11:20:18 +0300

    Adding server and client.

---
+++ Client/main.c
@@ -0,0 +1,75 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+#define BLOCK_SIZE 4096
+
+void func(int sockfd)
+{
+    printf("Into the func");
+    char buff[MAX];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+
+        int buffer[BLOCK_SIZE];
+        size_t bytes_read=-1;
+        buffer[BLOCK_SIZE] = malloc(BLOCK_SIZE);
+
+         while((bytes_read=read(sockfd, buffer, BLOCK_SIZE))>0)
+       {
+          printf("bytes_read=%d\n",bytes_read);
+       }
+        printf("From Server : %ls", buffer);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
+}
